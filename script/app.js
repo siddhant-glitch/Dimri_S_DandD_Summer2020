3a135f49ddcebc90ebac2538396bcc47923de2ad
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-05-29 13:00:30 -0400

    added basic drag and drop

--- script/app.js
+++ script/app.js
@@ -1,6 +1,8 @@
 (() => {
 	// set up the puzzle pieces and boards
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+	puzzlePieces = document.querySelectorAll(".puzzle-image"),
+	dropZones = document.querySelectorAll(".drop-zone"),
 	gameBoard = document.querySelector(".puzzle-board");
 
 
@@ -11,11 +13,31 @@
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
 	}
 
+	function allowDrag() {
+		//let the drag happens, and start a refrence of the ID of the elemnet we're dragging
+		console.log('started dragging an image: this one - ', event.target.id);
+
+	}
+
+	function allowDragOver(event) {
+		event.preventDefault(); //for the next week
+		console.log('dragged over me!')
+	}
+
 	// add event handling here -> how is the user going to use our app?
 	// what triggers do we need?
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
+
+	dropZones.forEach( zone => {
+			zone.addEventListener('dragover', allowDragOver);
+			zone.addEventListener('drop', allowDrop);
+	});
+
+
 
 	//research call,apply and bind
 	changeImageSet.call(puzzleButtons[0]); //emulates a click on the first bottom button
