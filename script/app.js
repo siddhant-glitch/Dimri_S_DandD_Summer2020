1c5ecb09f86b30548e3bce8f21dddaf8f1d3d7c2
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-05-29 12:21:42 -0400

    added matching background to gameboard

--- script/app.js
+++ script/app.js
@@ -1,12 +1,14 @@
 (() => {
 	// set up the puzzle pieces and boards
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+	gameBoard = document.querySelector(".puzzle-board");
+
 
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources,
-		// and set the drop zone background
 
-		debugger;
+		// and set the drop zone background
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
 	}
 
 	// add event handling here -> how is the user going to use our app?
@@ -14,4 +16,7 @@
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	//research call,apply and bind
+	changeImageSet.call(puzzleButtons[0]); //emulates a click on the first bottom button
 })();
