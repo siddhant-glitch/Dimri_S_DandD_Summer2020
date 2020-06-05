5aa308b19232bb1ac14363e108573660b58f9045
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-06-05 11:41:50 -0400

    drag and drop working

--- script/app.js
+++ script/app.js
@@ -5,7 +5,6 @@
 	dropZones = document.querySelectorAll(".drop-zone"),
 	gameBoard = document.querySelector(".puzzle-board");
 
-
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources,
 
@@ -13,9 +12,12 @@
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
 	}
 
-	function allowDrag() {
+	function allowDrag(event) {
 		//let the drag happens, and start a refrence of the ID of the elemnet we're dragging
 		console.log('started dragging an image: this one - ', event.target.id);
+
+		event.dataTransfer.setData("draggedImg", this.id); //setting data for draggedImg
+	//	event.dataTransfer.setData("targetTrack", this.dataset.track);   //for audio track
 
 	}
 
@@ -24,20 +26,33 @@
 		console.log('dragged over me!')
 	}
 
+	function allowDrop(event) {
+		console.log('dropped!');
+
+		let droppedImage	= event.dataTransfer.getData("draggedImg");
+	//	let currentTrack = event.dataTransfer.getData('targetTrack')   // for audio track
+		event.target.appendChild(document.querySelector(`${droppedImage}`));
+	}
 	// add event handling here -> how is the user going to use our app?
 	// what triggers do we need?
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
-
 	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 
-	dropZones.forEach( zone => {
-			zone.addEventListener('dragover', allowDragOver);
-			zone.addEventListener('drop', allowDrop);
-	});
+	// dropZones.forEach(zone => {
+	// 		zone.addEventListener('dragover', allowDragOver);
+	// 		zone.addEventListener('drop', allowDrop);
+	// });
 
-
+//rewrite this loop in an older style
+//for (var i = 0; i < dropZones.length; i++) {
+	//do a repeating action
+//}
+for (let zone of dropZones) {
+	zone.addEventListener('dragover', allowDragOver);
+	zone.addEventListener('drop', allowDrop);
+}
 
 	//research call,apply and bind
 	changeImageSet.call(puzzleButtons[0]); //emulates a click on the first bottom button
