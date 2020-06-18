f5823b33eb0b503dee3b59de1906eeabcd32880d
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-06-18 00:25:27 -0400

    solved the first bug(allowing only one puzzle piece to drop on the dropzone)

--- script/app.js
+++ script/app.js
@@ -1,15 +1,21 @@
 (() => {
 	// set up the puzzle pieces and boards
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+	//querySelectorAll is for a one to many relationship an dretyurns a nodeList(an array) of match.
 	puzzlePieces = document.querySelectorAll(".puzzle-image"),
 	dropZones = document.querySelectorAll(".drop-zone"),
 	gameBoard = document.querySelector(".puzzle-board");
 
+let imageNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources,
-
+		  imageNames.forEach((piece, index) => {
+			puzzlePieces[index].src = `images/${piece + this.dataset.bgkey}.jpg`;
+		});
 		// and set the drop zone background
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
+
 	}
 
 	function allowDrag(event) {
@@ -17,9 +23,9 @@
 		console.log('started dragging an image: this one - ', event.target.id);
 
 		event.dataTransfer.setData("draggedImg", this.id); //setting data for draggedImg
-	//	event.dataTransfer.setData("targetTrack", this.dataset.track);   //for audio track
-
+	//event.dataTransfer.setData("targetTrack", this.dataset.track);   //for audio track
 	}
+
 
 	function allowDragOver(event) {
 		event.preventDefault(); //for the next week
@@ -27,15 +33,23 @@
 	}
 
 	function allowDrop(event) {
-		console.log('dropped!');
+		
 
-		let droppedImage	= event.dataTransfer.getData("draggedImg");
+		if(!this.hasChildNodes()){
+			event.preventDefault();
+			console.log('dropped!');
+
+			let droppedImage = event.dataTransfer.getData("draggedImg");
+
+			event.target.appendChild(document.querySelector(`#${droppedImage}`));
+		}
+		
 	//	let currentTrack = event.dataTransfer.getData('targetTrack')   // for audio track
-		event.target.appendChild(document.querySelector(`#${droppedImage}`));
+		
 	}
+
 	// add event handling here -> how is the user going to use our app?
 	// what triggers do we need?
-
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
 	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
@@ -49,6 +63,7 @@
 //for (var i = 0; i < dropZones.length; i++) {
 	//do a repeating action
 //}
+
 for (let zone of dropZones) {
 	zone.addEventListener('dragover', allowDragOver);
 	zone.addEventListener('drop', allowDrop);
@@ -56,4 +71,6 @@ for (let zone of dropZones) {
 
 	//research call,apply and bind
 	changeImageSet.call(puzzleButtons[0]); //emulates a click on the first bottom button
+
 })();
+
